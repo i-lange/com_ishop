4a269afd4ea1f93be37ad4b98fc5f7d51e5bdbb2
Pavel Lange <pavel@ilange.ru>
2026-06-20 15:40:14 +0300

    Добавлен новый тип поля Modal_Manufacturer

---
+++ backend/tmpl/manufacturers/modal.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * @package    com_ishop
+ * @author     Pavel Lange <pavel@ilange.ru>
+ * @link       https://github.com/i-lange/com_ishop
+ * @copyright  (C) 2026 Pavel Lange <https://ilange.ru>
+ * @license    GNU General Public License version 2 or later
+ */
+
+defined('_JEXEC') or die;
+
+use Ilange\Component\Ishop\Site\Helper\RouteHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Multilanguage;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Layout\LayoutHelper;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Session\Session;
+
+/** @var Ilange\Component\Ishop\Administrator\View\Manufacturers\HtmlView $this */
+
+$app = Factory::getApplication();
+
+if ($app->isClient('site')) {
+    Session::checkToken('get') or die(Text::_('JINVALID_TOKEN'));
+}
+
+/** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
+$wa = $this->getDocument()->getWebAssetManager();
+$wa->useScript('core')
+    ->useScript('modal-content-select');
+
+$listOrder = $this->escape($this->state->get('list.ordering'));
+$listDirn  = $this->escape($this->state->get('list.direction'));
+$multilang = Multilanguage::isEnabled();
+?>
+<div class="container-popup">
+    <form action="<?php echo Route::_('index.php?option=com_ishop&view=manufacturers&layout=modal&tmpl=component&' . Session::getFormToken() . '=1'); ?>" method="post" name="adminForm" id="adminForm">
+        <?php echo LayoutHelper::render('joomla.searchtools.default', ['view' => $this]); ?>
+        <?php if (empty($this->items)) : ?>
+            <div class="alert alert-info">
+                <span class="icon-info-circle" aria-hidden="true"></span><span class="visually-hidden"><?php echo Text::_('INFO'); ?></span>
+                <?php echo Text::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
+            </div>
+        <?php else : ?>
+            <table class="table table-sm">
+                <caption class="visually-hidden">
+                    <?php echo Text::_('COM_ISHOP_MANUFACTURERS'); ?>,
+                    <span id="orderedBy"><?php echo Text::_('JGLOBAL_SORTED_BY'); ?> </span>,
+                    <span id="filteredBy"><?php echo Text::_('JGLOBAL_FILTERED_BY'); ?></span>
+                </caption>
+                <thead>
+                    <tr>
+                        <th scope="col" class="w-1 text-center">
+                            <?php echo HTMLHelper::_('searchtools.sort', 'JSTATUS', 'a.state', $listDirn, $listOrder); ?>
+                        </th>
+                        <th scope="col" class="title">
+                            <?php echo HTMLHelper::_('searchtools.sort', 'JGLOBAL_TITLE', 'a.title', $listDirn, $listOrder); ?>
+                        </th>
+                        <?php if ($multilang) : ?>
+                            <th scope="col" class="w-15">
+                                <?php echo HTMLHelper::_('searchtools.sort', 'JGRID_HEADING_LANGUAGE', 'language', $listDirn, $listOrder); ?>
+                            </th>
+                        <?php endif; ?>
+                        <th scope="col" class="w-10 d-none d-md-table-cell">
+                            <?php echo HTMLHelper::_('searchtools.sort', 'JDATE', 'a.created', $listDirn, $listOrder); ?>
+                        </th>
+                        <th scope="col" class="w-1 d-none d-md-table-cell">
+                            <?php echo HTMLHelper::_('searchtools.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                $iconStates = [
+                    -2 => 'icon-trash',
+                    0  => 'icon-unpublish',
+                    1  => 'icon-publish',
+                    2  => 'icon-archive',
+                ];
+                ?>
+                <?php foreach ($this->items as $i => $item) : ?>
+                    <?php
+                    $lang = '';
+                    if ($item->language && $multilang) {
+                        $tag = strlen($item->language);
+                        if ($tag == 5) {
+                            $lang = substr($item->language, 0, 2);
+                        } elseif ($tag == 6) {
+                            $lang = substr($item->language, 0, 3);
+                        }
+                    }
+
+                    $link     = RouteHelper::getManufacturerRoute($item->id, $item->language);
+                    $itemHtml = '<a href="' . $this->escape($link) . '"' . ($lang ? ' hreflang="' . $lang . '"' : '') . '>' . $item->title . '</a>';
+                    ?>
+                    <tr class="row<?php echo $i % 2; ?>">
+                        <td class="text-center">
+                            <span class="tbody-icon">
+                                <span class="<?php echo $iconStates[$this->escape($item->state)]; ?>" aria-hidden="true"></span>
+                            </span>
+                        </td>
+                        <th scope="row">
+                            <?php $attribs = 'data-content-select data-content-type="com_ishop.manufacturer"'
+                                . ' data-id="' . $item->id . '"'
+                                . ' data-title="' . $this->escape($item->title) . '"'
+                                . ' data-uri="' . $this->escape($link) . '"'
+                                . ' data-language="' . $this->escape($lang) . '"'
+                                . ' data-html="' . $this->escape($itemHtml) . '"';
+                            ?>
+                            <a class="select-link" href="javascript:void(0)" <?php echo $attribs; ?>>
+                                <?php echo $this->escape($item->title); ?>
+                            </a>
+                            <div class="small break-word">
+                                <?php if (empty($item->note)) : ?>
+                                    <?php echo Text::sprintf('JGLOBAL_LIST_ALIAS', $this->escape($item->alias)); ?>
+                                <?php else : ?>
+                                    <?php echo Text::sprintf('JGLOBAL_LIST_ALIAS_NOTE', $this->escape($item->alias), $this->escape($item->note)); ?>
+                                <?php endif; ?>
+                            </div>
+                        </th>
+                        <?php if ($multilang) : ?>
+                            <td class="small">
+                                <?php echo LayoutHelper::render('joomla.content.language', $item); ?>
+                            </td>
+                        <?php endif; ?>
+                        <td class="small d-none d-md-table-cell">
+                            <?php echo $item->created > 0 ? HTMLHelper::_('date', $item->created, Text::_('DATE_FORMAT_LC4')) : '-'; ?>
+                        </td>
+                        <td class="small d-none d-md-table-cell">
+                            <?php echo (int) $item->id; ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+            <?php echo $this->pagination->getListFooter(); ?>
+        <?php endif; ?>
+        <?php echo $this->filterForm->renderControlFields(); ?>
+    </form>
+</div>
